a1feaf66a12ad644ea662e3fd4cb4a281aca9bf3
Jun C <kjstudios858@gmail.com>
2018-10-19 19:50:37 -0700

    refactor Map URLs

--- basicforms/basicforms/urls.py
+++ basicforms/basicforms/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from basicapp import views
+
 
 urlpatterns = [
+    url(r'$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$formpage/', views.form_name_view, name='form_name')
 ]
